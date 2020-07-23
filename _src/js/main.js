1389f19f7667b10c81df28d9c9c5c21c27dc80f6
Zhenia <zheniaanfilatov@gmail.com>
2020-07-23 18:32:15 +0300

    create: add hotel-right, map and media

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
-  $('.slick-slider').slick({
 
-  });
+  // слайдер
+  $('.slick-slider').slick({});
+
+
+  // карты
+  ymaps.ready(init);
+
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+      center: [7.973430, 98.343838],
+      zoom: 15
+    });
+    // Создание геообъекта с типом точка (метка).
+    var myGeoObject = new ymaps.GeoObject({
+      geometry: {
+        type: "Point", // тип геометрии - точка
+        coordinates: [7.973430, 98.343838] // координаты точки
+      }
+    });
+
+    // Размещение геообъекта на карте.
+    myMap.geoObjects.add(myGeoObject);
+  }
 });
